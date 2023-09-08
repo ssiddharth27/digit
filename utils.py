b838767f68146b7d46472f829ef1c4b3b63485b5
Siddharth <singh.133@iitj.ac.in>
2023-09-08 05:36:01 +0530

    changes done

--- utils.py
+++ utils.py
@@ -1,4 +1,4 @@
-from sklearn import metrics
+from sklearn import datasets, metrics, svm
 from sklearn.model_selection import train_test_split
 
 def preprocess_data(data):
@@ -26,29 +26,61 @@ def predict_and_eval(model, X_test, y_test):
    # prediction
    predicted = model.predict(X_test)
    
+   accuracy = metrics.accuracy_score(y_test, predicted)
+   
+   return accuracy
+   
+def tune_hparams(X_train, Y_train, X_dev, y_dev, list_of_all_param_combinations):
+
+   best_acc = -1  
+   hyperparameters = {'gamma':[0.001,0.01,0.1,1,10,100],"c_ranges":[0.1,1,2,5,10]} 
+   
+   for params in list_of_all_param_combinations:
+
+        hyperparameter_settings = dict(zip(hyperparameters.keys(), params))
+
+        cur_gamma = hyperparameter_settings['gamma']
+        cur_crange = hyperparameter_settings['c_ranges']
+	
+        cur_model = svm.SVC(gamma=cur_gamma,C = cur_crange)
+        cur_model.fit(X_train, Y_train)
+
+        cur_dev_acc = predict_and_eval(cur_model, X_dev, y_dev)
+        if cur_dev_acc > best_acc:
+            best_acc = cur_dev_acc
+            best_gamma = cur_gamma
+            best_crange = cur_crange
+            best_model = cur_model
+	    
+   return best_gamma, best_crange, best_model, best_acc
+	    
+	 
+
+	#print(f"test_size={cur_test_size} dev_size={cur_dev_size} train_size={cur_train_size} train_acc={train_acc:.2f}     dev_acc={dev_acc:.2f} test_acc={test_acc:.2f}"  )
+   
    # metrics
-   print(
-    f"Classification report for classifier {model}:\n"
-    f"{metrics.classification_report(y_test, predicted)}\n"
-    ) 
+   #print(
+    #f"Classification report for classifier {model}:\n"
+   # f"{metrics.classification_report(y_test, predicted)}\n"
+    #) 
     
-   disp = metrics.ConfusionMatrixDisplay.from_predictions(y_test, predicted)
-   disp.figure_.suptitle("Confusion Matrix")
-   print(f"Confusion matrix:\n{disp.confusion_matrix}")
+   #disp = metrics.ConfusionMatrixDisplay.from_predictions(y_test, predicted)
+   #disp.figure_.suptitle("Confusion Matrix")
+   #print(f"Confusion matrix:\n{disp.confusion_matrix}")
 
    # The ground truth and predicted lists
-   y_true = []
-   y_pred = []
-   cm = disp.confusion_matrix
+   #y_true = []
+   #y_pred = []
+   #cm = disp.confusion_matrix
 
    # For each cell in the confusion matrix, add the corresponding ground truths
    # and predictions to the lists
-   for gt in range(len(cm)):
-      for pred in range(len(cm)):
-          y_true += [gt] * cm[gt][pred]
-          y_pred += [pred] * cm[gt][pred]
-   print(
-    "Classification report rebuilt from confusion matrix:\n"
-    f"{metrics.classification_report(y_true, y_pred)}\n"
-   )
+   #for gt in range(len(cm)):
+    #  for pred in range(len(cm)):
+     #     y_true += [gt] * cm[gt][pred]
+      #    y_pred += [pred] * cm[gt][pred]
+   #print(
+    #"Classification report rebuilt from confusion matrix:\n"
+    #f"{metrics.classification_report(y_true, y_pred)}\n"
+   #)
     
