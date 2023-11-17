557e3931462bf74a551d410da3620d1c47f2ec56
Siddharth <singh.133@iitj.ac.in>
2023-11-17 13:50:46 +0530

    newch

--- api/digit_pred.py
+++ api/digit_pred.py
@@ -2,9 +2,9 @@ from flask import Flask, request
 from joblib import dump, load
 import numpy as np
 
+
 app = Flask(__name__)
 
-model = load('/home/siddharth/Documents/Ml-ops/Digit_classification/digit/models/svm_gamma:0.001_C:1.joblib')
 
 
 @app.route('/compare_images', methods=['POST'])
@@ -13,12 +13,17 @@ def compare_images():
     js = request.get_json()
     image1 = js['image1']
     image2 = js['image2']
-
-    prediction1 = model.predict(image1)
-    prediction2 = model.predict(image2)
+    print("hello ")
+    
+    img1 = np.array([float(i) for i in image1]).reshape(1,-1)
+    img2 = np.array([float(i) for i in image2]).reshape(1,-1)
+    
+    model = load('/home/siddharth/Documents/Ml-ops/Digit_classification/digit/models/svm_gamma:0.001_C:1.joblib')
+    prediction1 = model.predict(img1)
+    prediction2 = model.predict(img2)
 
     # Compare the predicted digits
     if np.argmax(prediction1) == np.argmax(prediction2):
-        return True
+        return "True"
     else:
-        return False
+        return "False"
