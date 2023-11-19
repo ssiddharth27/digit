054950623da35df4218fc5d3569d77d3791cee7e
Siddharth <singh.133@iitj.ac.in>
2023-11-19 16:48:31 +0530

    quiz4

---
+++ test_flask.py
@@ -0,0 +1,45 @@
+from api.app import app
+from sklearn import datasets, metrics, svm
+import pytest
+
+def test_get_root():
+	response = app.test_client().get("/")
+	assert response.status_code == 200
+	assert response.get_data() == b"<p>Hello, World!</p>"
+
+def test_post_root():
+
+	suffix = "post suffix"
+	response = app.test_client().post("/", json={"suffix": suffix})
+	assert response.status_code == 200    
+	assert response.get_json()['op'] == "Hello, World POST " + suffix
+
+def test_post_predict():
+
+        digit_samples = []
+        digits = datasets.load_digits()
+
+        
+        for i in range(10):
+        
+            digit_indices = (digits.target == i)
+            digit_sample = digits.data[digit_indices][0]
+            digit_samples.append(list(digit_sample))
+        
+        
+        sample_payloads = [{"data": digit_samples[0]}, {"data": digit_samples[1]}, {"data": digit_samples[2]},
+	                   {"data": digit_samples[3]}, {"data": digit_samples[4]}, {"data": digit_samples[5]},
+	                   {"data": digit_samples[6]}, {"data": digit_samples[7]}, {"data": digit_samples[8]},
+	                   {"data": digit_samples[9]}]
+	                   
+        for digit, payload in enumerate(sample_payloads):
+            
+            with app.test_client() as client:
+                response = client.post("/predict",json=payload)
+                #assert response.status_code == 200
+                assert int(response.get_data(as_text = True)) == digit
+                assert response.status_code == 200
+	
+
+
+
